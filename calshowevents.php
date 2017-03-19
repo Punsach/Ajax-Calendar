61865d04769e627bd58c29be48d14d64643b37d5
Puneet Sachdeva <puneet.sachdeva@wustl.edu>
2017-03-18 19:32:04 -0500

    Edited show events

--- calshowevents.php
+++ calshowevents.php
@@ -1,6 +1,7 @@
 <?php
 	header("Content-Type: application/json");
     require 'caldatabase.php';   
+    $myArray = array();
     session_start();              
     $user = $_SESSION['user_id'];
     $date = $_POST['date'];
@@ -14,14 +15,10 @@
     $stmt->execute();
     $stmt->bind_result($id, $time, $title); 	
 	while($row = $stmt->fetch()){
-  		$json[]['id'] = $id;
-  		//$json[]['date'] = $date;
-  		$json[]['time'] = $time;
-  		$json[]['title'] = $title;
+  		$tempArray = $row;
+      array_push($myArray, $tempArray);
 	}
-	echo json_encode($json);   
+	echo json_encode($myArray);   
    $stmt->close();
-
-
-
+   
 ?>
